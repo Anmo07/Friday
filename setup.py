dc8959d94361eb8b578c3896dcdefb0f2a6f1bbb
Anmol <anmolrajotiya@gmail.com>
2026-04-30 21:34:48 +0530

    chore: bump version to 0.1.1 for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="friday",
-    version="0.1.0",
+    version="0.1.1",
     package_dir={"": "friday"},
     packages=find_packages(where="friday"),
 )
